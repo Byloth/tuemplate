e874a509392bf0ad22f59474c78e19dd42cbb2dd
Matteo Bilotta <me@byloth.dev>
2024-01-29 12:31:54 +0100

    fix: Fixed `BASE_URL` for correct GitHub Pages deploy.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/tuemplate/",
   plugins: [Vue()],
   resolve: {
     alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) }
